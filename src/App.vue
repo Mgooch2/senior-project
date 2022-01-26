0384afe815b6f82addd618477a960f9ea5d3c521
Mgooch2 <86744580+Mgooch2@users.noreply.github.com>
2022-01-26 18:45:08 -0500

    Mionne- added Footer component

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Footer from "@/components/Footer";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Footer
   }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer>
+    <p>&#169; 2022 Total Health portal</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped>
+footer{
+  background-color: #133576;
+  padding: 10px;
+  text-align: center;
+  color: white;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
